293dafcc902a39825e425ebba74659d48ac5c7d8
ask <argo@argosoft.pl>
2022-02-13 20:37:19 +0100

    q

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("New Branch.");
+        System.out.println("test");
     }
 }
